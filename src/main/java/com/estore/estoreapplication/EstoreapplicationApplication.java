68d0f30a68abbb60621be253582882ec981c6ade
Darshan <darshan.kb@outlook.com>
2023-04-03 12:09:03 +0530

    estore project created

---
+++ src/main/java/com/estore/estoreapplication/EstoreapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.estore.estoreapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoreapplicationApplication.class, args);
+	}
+
+}
