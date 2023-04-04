cd53f786e4e1cfe2d0f6b91108ee7c525d5c4275
unknown <premarajkolte@gmail.com>
2023-04-04 18:19:20 +0530

    C:/Program Files/Git/home

--- src/main/java/com/estore/estoreapplication/controller/HomeController.java
+++ src/main/java/com/estore/estoreapplication/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
-	@RequestMapping("/Home")
+	@RequestMapping("/home")
 	public String Home() {
 		return "EstoreHome.js";
 	}
